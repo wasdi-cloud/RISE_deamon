e4422bcf1a5b0872c7ededd692fdc6db98aa4261
FADEOUT\p.campanella <p.campanella@fadeout.it>
2025-05-08 17:47:03 +0200

    no message

--- src/rise/RiseDeamon.py
+++ src/rise/RiseDeamon.py
@@ -36,7 +36,7 @@ class RiseDeamon:
         :return:
         """
 
-        logging.info("RiseDeamon.run: Rise deamon start v.1.2.1")
+        logging.info("RiseDeamon.run: Rise deamon start v.1.2.2")
 
         logging.getLogger("requests").propagate = False
         logging.getLogger("urllib3").propagate = False
